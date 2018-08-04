24dcfaa93e69e844e06c6839651c2efeed630f5d
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-04 08:42:34 +0000

    modified:   main.c

--- main.c
+++ main.c
@@ -2,15 +2,27 @@
 
 // has clock_init() but nothing for the uart - 04:44 UTC 04 AUG 2018
 
+#include "sam.h"
 #include "atmel_start.h"
 #include "clock_init.h"
 #include "led_pulse.h"
+#include "uart_setup.h"
+#include "gpio_local.h"
+
+extern void jake_uart(void); // no .h file haha
 
 /* Many changes: by wa1tnr, July 2018 */
 
 int main(void) {
     SystemInit();
     clock_init();
+    init_act_LED();
+    SysTick_Config(5000000);
+
+    jake_uart();
+
+    while(1);
+
     led_pulse();
     while (1) {
         // none
