6b1807a60886788c7aba5a6f88ea68b5aeb2868d
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-04 06:03:12 +0000

    comments removed 	modified:   blinker.h

--- blinker.h
+++ blinker.h
@@ -1,33 +1,4 @@
-// blinker.c ainsuForth D51
-
-// #include "atmel_start.h"
-// #include "gpio_local.h"
-// #include "uartport.h"
-// #include "hardware.h"
-
-/* Many changes: by wa1tnr, July 2018 */
-
-
-/*
-void uSec(void) { }
-*/
-
+// blinker.h ainsuForth D51
 extern void short_timer(void);
 extern void raise_LED_pins(void);
-
-/*
-
-void lower_LED_pins(void) { }
-
-void blink_LED(void) { }
-
-*/
 extern void flicker_LED(void);
-/*
-
-void blinkLEDfast(void) { }
-
-void activity_LED_demo(void) { }
-
-void long_long_timer(void) { }
-*/
