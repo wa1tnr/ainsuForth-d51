b8479a7e8e15d9dcd531630dec5b84ba063025f6
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-04 10:19:05 +0000

    modified:   main.c

--- main.c
+++ main.c
@@ -8,23 +8,36 @@
 #include "led_pulse.h"
 #include "uart_setup.h"
 #include "gpio_local.h"
+#include "jake_uart.h"
+#include "blinker.h"
 
 extern void jake_uart(void); // no .h file haha
 
 /* Many changes: by wa1tnr, July 2018 */
 
 int main(void) {
+    uint8_t value = 0;
     SystemInit();
     clock_init();
-    init_act_LED();
     SysTick_Config(5000000);
-
+    init_act_LED();
+    gpio_set_pin_level(PA23, true);
+    gpio_set_pin_level(PA23, false);
+    // while(1);
     jake_uart();
+    // THIS // while(1);
 
-    while(1);
+    gpio_set_pin_level(PA23, true); short_timer(); gpio_set_pin_level(PA23, false); short_timer();
+    gpio_set_pin_level(PA23, true); short_timer(); gpio_set_pin_level(PA23, false); short_timer();
+    gpio_set_pin_level(PA23, true); short_timer(); gpio_set_pin_level(PA23, false); short_timer();
 
-    led_pulse();
+    // led_pulse();
     while (1) {
-        // none
+        value = byte_seen * 2; // arbitrary scale
+        // delay_ms(value); // a form of output - a delay
+        for(uint8_t i = value; i > 0; i--) {
+            PORT->Group[0].OUTTGL.reg = (uint32_t)(1 << 23); // blink D13 / PA23
+            short_timer();
+        } // toggle port pin on and off to represent the byte seen
     }
 }
