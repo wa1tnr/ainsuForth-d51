92cb5701bcfa1138560763e923c663f68b0a7c3a
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-04 04:38:33 +0000

    compress out removed code lines to just their headings 	modified:   main.c

--- main.c
+++ main.c
@@ -10,60 +10,15 @@
 /* Many changes: by wa1tnr, July 2018 */
 
 /*
-void uSec(void) {
-    for (volatile int i = 1; i < 2; i++) {
-        // nothing
-    }
-}
-
-void short_timer(void) {
-    uint32_t on_time  = 2140111222; // it's 2147 something ;)
-    for(int j = 2; j>0; j--) {
-        for(on_time =       2140112; on_time > 0; on_time--) { // 21.4 million
-            uSec();
-        }
-    }
-}
-
-void raise_LED_pins(void) {
-    raise_D13_feather();
-}
-
-void lower_LED_pins(void) {
-    lower_D13_feather();
-}
-
-void blink_LED(void) {
-    raise_LED_pins();
-    short_timer();
-    lower_LED_pins();
-    short_timer();
-}
-
-void flicker_LED(void) {
-    raise_LED_pins();
-    short_timer();
-    lower_LED_pins();
-    short_timer();
-}
-
-void blinkLEDfast(void) {
-    for(int i = 15; i > 0; i--) {
-        blink_LED();
-    }
-}
-
-void activity_LED_demo(void) {
-    blinkLEDfast();
-}
-
-void long_long_timer(void) {
-    for (int j=4; j>1; j--){
-        for (int i=3; i>1; i--){
-            short_timer();
-        }
-    }
-}
+void uSec(void) { }
+void short_timer(void) { } } }
+void raise_LED_pins(void) { } 
+void lower_LED_pins(void) { } 
+void blink_LED(void) { }
+void flicker_LED(void) { }
+void blinkLEDfast(void) { } }
+void activity_LED_demo(void) { }
+void long_long_timer(void) { }
 
 */
 #ifdef NEVER_EEE
