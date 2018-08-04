da313c30a7158ea02ff7c467ca805dedb76e9353
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-04 06:01:22 +0000

    Code cleanup 	modified:   main.c

--- main.c
+++ main.c
@@ -3,27 +3,15 @@
 // has clock_init() but nothing for the uart - 04:44 UTC 04 AUG 2018
 
 #include "atmel_start.h"
-#include "gpio_local.h"
-#include "uartport.h"
-#include "hardware.h"
-#include "blinker.h"
 #include "clock_init.h"
+#include "led_pulse.h"
 
 /* Many changes: by wa1tnr, July 2018 */
-
-void nmain(void) {
-        raise_LED_pins();
-	while (1) {
-            flicker_LED();
-            short_timer();
-	}
-}
 
 int main(void) {
     SystemInit();
     clock_init();
-    init_act_LED();
-    nmain();
+    led_pulse();
     while (1) {
         // none
     }
