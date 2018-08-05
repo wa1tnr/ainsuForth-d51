14b2734ee5af885aac85c36a16e465e6f30d8429
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-05 10:40:00 +0000

    new file:   portable.d/blinker.h

---
+++ portable.d/blinker.h
@@ -0,0 +1,6 @@
+// blinker.h ainsuForth D51
+extern void short_timer(void);
+extern void raise_LED_pins(void);
+extern void flicker_LED(void);
+extern void blink_LED(void);
+extern void calibrate_usec(void); // { // endless loop
